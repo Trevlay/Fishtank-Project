63974eebe5a674954d276b97c9c360716b8a9af5
Carli Whitfield Hooten <singingcoconut@hotmail.com>
2016-01-20 20:24:43 -0600

    Update projectfishtank.ino

--- projectfishtank.ino
+++ projectfishtank.ino
@@ -1,5 +1,17 @@
-double salinity;
-int output;
+int Csp=0;       //Setpoint
+int C1=0;        //Initial NaCl concentration 
+int C2=0;        //Target NaCl concentration
+int deadtime=0;  //Deadtime
+int FR=0;        //Flow rate through solenoid valve
+int G=0;         //Gain
+int OF=0;        //Fraction of overflow that comes from DI tank
+int output=0;    //Number returned from the analogRead()
+int m=0;         //Mass of water in fishtank basin
+int x=0;         //Mass of correction water to be added
+int t=0;         //Time since the last valve was opened
+int tlast=0;     //Time since last valve opened in milliseconds
+int topen=0;     //computed time for valve to stay open in ms
+double salinity=0;
 
 void setup()
 {
