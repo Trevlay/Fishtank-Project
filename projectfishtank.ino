8b749c02794e29dc17c1822297de033e1712e00f
Trevor Layman <trevty444@yahoo.com>
2016-01-20 20:21:32 -0600

    Update projectfishtank.ino

--- projectfishtank.ino
+++ projectfishtank.ino
@@ -1,8 +1,22 @@
-int target=0;
+double salinity;
+int output;
 
-void setup(){
-  
+void setup()
+{
+Serial.begin(9600);
+
 }
-void loop(){
-  
+
+void loop()
+{
+// output=analogRead (0);
+output=500;
+salinity= 1.6671E-17*pow(output,5.0994);
+Serial.print("output= "); Serial.print(output);
+Serial.print("    salinity= "); Serial.println(salinity,21);
+
+
+// In setup, compute UCL and LCL given a certain setpoint. Then, in loop, create if(salinity>=[UCL/LCL]) x2 and input
+// solenoid valve program ~should be on Carli's pc or on pp.
+
 }
